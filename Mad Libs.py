8f505afbcd59329e12ba5dbf8a40703df34a3104
Calvin <46201774+calvinjlzhai@users.noreply.github.com>
2019-03-28 00:20:25 -0400

    Add files via upload
    Using basic python to a simple mad libs about school kids.

---
+++ Mad Libs.py
@@ -0,0 +1,33 @@
+#Set up the variable for user input
+
+adjective = raw_input("Enter a adjective term: ");
+adjective1 = raw_input("Enter a adjective term: ");
+food1 = raw_input("Enter a type of food: ");
+food2 = raw_input("Enter a type of food: ");
+food3 = raw_input("Enter a type of food: ");
+food4 = raw_input("Enter a type of food: ");
+food5 = raw_input("Enter a type of food: ");
+verb = raw_input("Enter a verb term: ");
+verb1 = raw_input("Enter a verb term: ");
+verb2= raw_input("Enter a past tense verb term: ")
+noun = raw_input("Enter a noun term: ");
+noun1 = raw_input("Enter a noun term: ");
+vegetable = raw_input("Enter a type of vegetable: ");
+fruit = raw_input("Enter a type of fruit: ");
+furniture = raw_input("Enter a type of furniture: ");
+emotion = raw_input("Enter a type of emotion: ");
+
+
+print("Lunchtime in our cafeteria is always "+ adjective + ". " +
+      "They serve hot " + food1 + " and " + food2 + ", but some students " + verb +
+      " their own " + noun + " to eat. ");
+print("Some kids quietly " + verb1 + " their " + food3 +
+      " ,while others throw " + vegetable + " or " + fruit + " when the teacher aren\'t looking.");
+print("One time, a bunch of kids mixed all of their unfinished " + food4 + " and " + food5 +
+      " together to make a " + adjective1 + " mountain of " + noun1 + " on a " + furniture + ".");
+print("The teacher were " + emotion + ", but everyone had already " + verb2 +
+      " outside of recess, so no body got in trouble.");
+
+
+
+
